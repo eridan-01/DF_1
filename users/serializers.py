3ee4a46ae4fc360e1331168262740ee3d97116ee
Eugene Geym <geymzheka@gmail.com>
2024-09-04 01:56:23 +0500

    changed serializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,4 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['first_name', 'last_name', 'email', 'phone_number']
