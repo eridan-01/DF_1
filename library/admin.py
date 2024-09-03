3a6eff1d579c1cefb9a1834cf681ad80e41fba6e
Eugene Geym <geymzheka@gmail.com>
2024-09-04 00:05:38 +0500

    added admin register for library

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Author, Book, BookIssue
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'published_date', 'available_copies')
+
+
+@admin.register(BookIssue)
+class BookIssueAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'borrow_date', 'due_date', 'return_date', 'status')
