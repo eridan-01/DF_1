4c8fcbc3a4ca45705d3517d5c998a07466f39431
Eugene Geym <geymzheka@gmail.com>
2024-09-10 20:54:45 +0500

    added is staff

--- users/permissions.py
+++ users/permissions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class IsModer(BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name="moders").exists()
+        return request.user.groups.filter(name="moders").exists() or request.user.is_staff
